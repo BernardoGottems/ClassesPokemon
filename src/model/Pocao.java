bdf26642336b51865be8b321f65bf77f49d3b5ac
heloisa-flores <heloisaflores.cwb@gmail.com>
2026-06-12 10:45:15 -0300

    Extend Pocao class from ItemBase and add healing points

--- src/model/Pocao.java
+++ src/model/Pocao.java
@@ -1,4 +1,16 @@
 package model;
 
-public class Pocao {
+public class Pocao extends ItemBase{
+
+    private int pontosDeCura;
+
+    public Pocao(String nome, int quantidade, double precoBase, int pontosDeCura) {
+        super(nome, quantidade, precoBase);
+        this.pontosDeCura = pontosDeCura;
+    }
+
+    public int getPontosDeCura() {
+        return pontosDeCura;
+    }
 }
+
