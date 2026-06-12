46d3e6ebdded649c8c0770a5738907b6ff66a5ab
Marcelinho <mh390150@gmail.com>
2026-06-12 18:32:42 -0300

    Add files via upload

--- src/view/MapaView.java
+++ src/view/MapaView.java
@@ -1,16 +1,49 @@
 package view;
 
+import java.util.Scanner;
+
 public class MapaView {
-    public void atualizarMapa(char[][] matriz, int jogadorX, int jogadorY){
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (j == jogadorX && i == jogadorY) {
-                    System.out.print("P ");
-                } else {
-                    System.out.print(matriz[i][j] + " ");
+    Scanner scan = new Scanner(System.in);
+    public static final String RESET = "\u001B[0m";
+    public static final String VERMELHO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARELO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String ROXO = "\u001B[35m";
+    public static final String CIANO = "\u001B[36m";
+
+    public char Imprimir(char[][] mapa){
+        for(int i = 0; i< 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(mapa[i][j] == 'H'){
+                    System.out.print(VERMELHO + mapa[i][j]);
+
+                }else if(mapa[i][j] == 'L'){
+                    System.out.print(AMARELO + mapa[i][j]);
+                }else if(mapa[i][j] == 'G'){
+                    System.out.print(ROXO + mapa[i][j]);
+                }else if(mapa[i][j] == '#'){
+                    System.out.print(VERDE + mapa[i][j]);
+                }
+                else {
+                    System.out.print(mapa[i][j]);
                 }
             }
-            System.out.println();
+            System.out.print("\n");
         }
+        System.out.println("Escolha para que direção voce quer ir(W/A/S/D)");
+        String escolha;
+        do {
+            escolha = scan.nextLine().toUpperCase();
+            if(escolha.length() != 1){
+                System.out.println("coloque apenas um caractere");
+            }else if(escolha.equals("W") || escolha.equals("A") || escolha.equals("D") || escolha.equals("S")){
+                return escolha.charAt(0);
+
+            } else{
+                System.out.println("entrada invalida");
+            }
+        }while(true);
+
     }
 }
