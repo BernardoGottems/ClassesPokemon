ea9987f51324e11dfaab112490f0cafc76a7f099
heloi <heloisaflores.cwb@gmail.com>
2026-06-12 11:04:36 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import controller.*;
 import model.*;
 import view.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
         ExploracaoController jogo = new ExploracaoController();
@@ -10,4 +12,21 @@ public class Main {
 
 
     }
+
+
+    // Criando o Pokémon (Membro 4)
+    PokemonBase pikachu = new PokemonBase("Pikachu", 30, 100); // Nome, HP Atual, HP Máximo
+
+
+    ArrayList<ItemBase> mochila = new ArrayList<>();
+    Pocao superPocao = new Pocao("Super Potion", 3, 25.00, 50); // Cura 50 de HP
+mochila.add(superPocao);
+
+
+    ItemView iv = new ItemView();
+iv.exibirMochila(mochila);
+
+
+    PocaoController pc = new PocaoController();
+pc.aplicarPocao(superPocao, pikachu);/*
 }
--- src/controller/PocaoController.java
+++ src/controller/PocaoController.java
@@ -39,20 +39,4 @@ public class PocaoController {
 }
 
 
-/* como ficará a parte da main (membro 5):
-// Criando o Pokémon (Membro 4)
-PokemonBase pikachu = new PokemonBase("Pikachu", 30, 100); // Nome, HP Atual, HP Máximo
-
-// Criando a mochila e adicionando suas poções (Membro 5)
-ArrayList<ItemBase> mochila = new ArrayList<>();
-Pocao superPocao = new Pocao("Super Potion", 3, 25.00, 50); // Cura 50 de HP
-mochila.add(superPocao);
-
-// Exibindo a mochila (Membro 5 - ItemView)
-ItemView iv = new ItemView();
-iv.exibirMochila(mochila);
-
-// Usando a Poção no Pikachu (Membro 5 - PocaoController)
-PocaoController pc = new PocaoController();
-pc.aplicarPocao(superPocao, pikachu);/*
 
