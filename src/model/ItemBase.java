491b0fe065beabc8c7485eada3fe511d2661d8ff
heloisa-flores <heloisaflores.cwb@gmail.com>
2026-06-12 10:43:45 -0300

    Refactor ItemBase to a concrete class with properties

--- src/model/ItemBase.java
+++ src/model/ItemBase.java
@@ -1,4 +1,32 @@
 package model;
 
-public abstract class ItemBase {
+public class ItemBase {
+
+    protected String nome;
+    protected int quantidade;
+    protected double precoBase;
+
+    public ItemBase(String nome, int quantidade, double precoBase) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.precoBase = precoBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+
 }
