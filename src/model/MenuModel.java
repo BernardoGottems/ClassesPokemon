98e91181b7f61a6d2cb5c6574b118e3372945489
Marcelinho <mh390150@gmail.com>
2026-06-13 10:39:05 -0300

    Model do menu adicionado

---
+++ src/model/MenuModel.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MenuModel {
+
+    public boolean rodando(int escolha){
+        if (escolha == 1) {
+
+            return true;
+        }
+        return false;
+    }
+}
